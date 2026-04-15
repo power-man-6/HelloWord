d4f38234bd08f12081948156579585372c4c5557
zhang <2392701369@qq.com>
2026-04-15 22:26:48 +0800

    实现函数

--- main.cpp
+++ main.cpp
@@ -3,13 +3,25 @@ using namespace std;
 
 void sort(int arr[], int size)
 {
-
+	int tmp=0;
+	for(int i=0;i<size-1;++i)
+	{
+		for(int j=0;j<size-i;++j)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				tmp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=tmp;
+			}
+		}
+	}
 }
 int main()
 
 {
-	int arrp[] = { 12,2,454,66,78,45,2, 7};
-	int size = sizeof(arrp) / sizeof(arrp[0]);
+	int arr[] = { 12,2,454,66,78,45,2, 7};
+	int size = sizeof(arr) / sizeof(arr[0]);
 	sort(arr, size);
 	return 0;
 }
