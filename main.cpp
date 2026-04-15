a6cfeb9cda75f3b3b6c24d3d4efc14cb5993dba0
ever <2392701369@qq.com>
2026-04-15 22:11:22 +0800

    创建main，写了初始代码

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include<iostream>
 using namespace std;
 
-void sort(int arr[],int size)
+void sort(int arr[], int size)
 {
-	
+
 }
 int main()
+
 {
-	int arr[]={12,4,89,21,}
-	int size=sizeof(arr)/sizeof(arr[0]);
-	sort(arr,size);
+	int arrp[] = { 12,2,454,66,78,45,2, 7};
+	int size = sizeof(arrp) / sizeof(arrp[0]);
+	sort(arr, size);
 	return 0;
 }
